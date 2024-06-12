af01c9edb348892d6a8ed5b7726660f5e61c5b94
Solomiia Lomnytska <solomiia.lomnytska.pz.2021@lpnu.ua>
2024-06-12 21:12:20 +0300

    Added messages area component

--- ai-chat-fe/src/pages/main-page/MainPage.jsx
+++ ai-chat-fe/src/pages/main-page/MainPage.jsx
@@ -1,15 +1,44 @@
 import React, { useState } from 'react';
 import './MainPage.css';
 import MessageInput from './components/MessageInput/MessageInput';
+import MessageArea from './components/MessageArea/MessageArea';
 
+const messages = [
+    {
+      _id: '1',
+      user: '123',
+      chat: '456',
+      text: 'Hello!',
+      role: 'user',
+      createdAt: '2024-06-12T10:00:00Z',
+    },
+    {
+      _id: '2',
+      user: '123',
+      chat: '456',
+      text: 'How can I help you? Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      role: 'tool',
+      createdAt: '2024-06-12T10:01:00Z',
+    }
+  ];
+
+  
 function MainPage() {
   const [sideMenuVisible, setSideMenuVisible] = useState(true);
 
   return (
     <div className={`main-page ${sideMenuVisible ? 'with-side-menu' : 'without-side-menu'}`}>
-      <div className='photo'></div>
+      <div className='header'> 
+        <div className='title'>
+            <h1>ALPHA AI</h1>
+        </div>
+        <div className='photo'></div>
+      </div>
+
       {sideMenuVisible && <div className='side-menu'></div>}
-      <div className='messages-container'></div>
+      <div className='messages-container'>
+        <MessageArea messages={messages}></MessageArea>
+      </div>
       <div className='message-input'>
         <MessageInput />
       </div>
