837a91aae1500967efe419a1b9df6a5917e2546f
Solomiia Lomnytska <solomiia.lomnytska.pz.2021@lpnu.ua>
2024-06-16 09:58:16 +0300

    Added router

--- ai-chat-fe/src/App.js
+++ ai-chat-fe/src/App.js
@@ -1,10 +1,17 @@
+import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/login/LoginPage";
 import MainPage from "./pages/main-page/MainPage";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <div className="App">
-      <LoginPage />
+      <Routes>
+        <Route path="login" element={ <LoginPage/> } />
+        <Route path="chat" element={ <MainPage/> } />
+      </Routes>
+      <ToastContainer />
     </div>
   );
 }
